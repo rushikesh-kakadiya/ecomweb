7fb1011d0db0ba5a13fa8cf9d8279007c23e0618
rushikesh-kakadiya <rushikesh.kakadiya114652@marwadiuniversity.ac.in>
2025-01-29 23:22:29 +0530

    admin features added

--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const {
@@ -35,7 +35,7 @@ const {
   getSelectedCartItems,
   updateCartItemSelection,
   getUserShippingAddress,
-} = require('./controller'); // Importing all functions from a controller file
+} = require("./controller"); // Importing all functions from a controller file
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -44,58 +44,146 @@ router.use(passport.initialize());
 router.use(passport.session());
 
 // User Routes
-router.post('/users/register', registerUser);
-router.post('/users/login', loginUser);
-router.get('/users/profile', passport.authenticate('jwt', { session: false }), getUserProfile); // Protected route
-router.put('/users/profile', passport.authenticate('jwt', { session: false }), updateUserProfile); // Protected route
+router.post("/users/register", registerUser);
+router.post("/users/login", loginUser);
+router.get(
+  "/users/profile",
+  passport.authenticate("jwt", { session: false }),
+  getUserProfile
+); // Protected route
+router.put(
+  "/users/profile",
+  passport.authenticate("jwt", { session: false }),
+  updateUserProfile
+); // Protected route
+// Example endpoint in Express.js (Node.js)
+router.get(
+  "/users/role",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Assuming `req.user` contains user data after authentication
+    const role = req.user?.role; // Get user role from JWT payload or user data in DB
+    console.log(role);
+
+    if (role === "admin") {
+      return res.json({ isAdmin: true });
+    } else {
+      return res.json({ isAdmin: false });
+    }
+  }
+);
 
 // Product Routes
-router.get('/products', getAllProducts);
-router.get('/products/:id', getProductDetails);
-router.post('/products', upload.single("image_url"), createProduct);
-router.put('/products/:id', updateProduct);
-router.delete('/products/:id', deleteProduct);
+router.get("/products", getAllProducts);
+router.get("/products/:id", getProductDetails);
+router.post("/products", upload.single("image_url"), createProduct);
+router.put("/products/:id", updateProduct);
+router.delete("/products/:id", deleteProduct);
 
 // Cart Routes
-router.get('/cart', passport.authenticate('jwt', { session: false }), getUserCart); // Protected route
-router.post('/cart', passport.authenticate('jwt', { session: false }), addToCart); // Protected route
-router.put('/cart/:cartItemId', passport.authenticate('jwt', { session: false }), updateCartQuantity); // Protected route
-router.delete('/cart/:cartItemId', passport.authenticate('jwt', { session: false }), deleteCartItem); // Protected route
+router.get(
+  "/cart",
+  passport.authenticate("jwt", { session: false }),
+  getUserCart
+); // Protected route
+router.post(
+  "/cart",
+  passport.authenticate("jwt", { session: false }),
+  addToCart
+); // Protected route
+router.put(
+  "/cart/:cartItemId",
+  passport.authenticate("jwt", { session: false }),
+  updateCartQuantity
+); // Protected route
+router.delete(
+  "/cart/:cartItemId",
+  passport.authenticate("jwt", { session: false }),
+  deleteCartItem
+); // Protected route
 
 // Selected cart items route
-router.get('/cart/selected', passport.authenticate('jwt', { session: false }), getSelectedCartItems); // Protected route
-router.put('/cart/select/:cartItemId', passport.authenticate('jwt', { session: false }), updateCartItemSelection); // Protected route
+router.get(
+  "/cart/selected",
+  passport.authenticate("jwt", { session: false }),
+  getSelectedCartItems
+); // Protected route
+router.put(
+  "/cart/select/:cartItemId",
+  passport.authenticate("jwt", { session: false }),
+  updateCartItemSelection
+); // Protected route
 
 // User shipping address route
-router.get('/user/address', passport.authenticate('jwt', { session: false }), getUserShippingAddress); // Protected route
+router.get(
+  "/user/address",
+  passport.authenticate("jwt", { session: false }),
+  getUserShippingAddress
+); // Protected route
 
 // Order Routes
-router.post('/orders', passport.authenticate('jwt', { session: false }), createOrder); // Protected route
-router.get('/orders', passport.authenticate('jwt', { session: false }), getOrdersByUserId); // Protected route
-router.get('/orders/:id', passport.authenticate('jwt', { session: false }), getOrderDetails); // Protected route
-router.put('/orders/:id', passport.authenticate('jwt', { session: false }), updateOrderStatus); // Protected route
+router.post(
+  "/orders",
+  passport.authenticate("jwt", { session: false }),
+  createOrder
+); // Protected route
+router.get(
+  "/orders",
+  passport.authenticate("jwt", { session: false }),
+  getOrdersByUserId
+); // Protected route
+router.get(
+  "/orders/:id",
+  passport.authenticate("jwt", { session: false }),
+  getOrderDetails
+); // Protected route
+router.put(
+  "/orders/:id",
+  passport.authenticate("jwt", { session: false }),
+  updateOrderStatus
+); // Protected route
 
 // Category Routes
-router.get('/categories', getAllCategories);
-router.post('/categories', createCategory);
-router.put('/categories/:id', updateCategory);
-router.delete('/categories/:id', deleteCategory);
+router.get("/categories", getAllCategories);
+router.post("/categories", createCategory);
+router.put("/categories/:id", updateCategory);
+router.delete("/categories/:id", deleteCategory);
 
 // Payment Routes
-router.post('/payments', makePayment);
+router.post("/payments", makePayment);
 
 // Review Routes
-router.get('/products/:id/reviews', getProductReviews);
-router.post('/products/:id/reviews', addReview);
-router.delete('/reviews/:id', deleteReview);
+router.get("/products/:id/reviews", getProductReviews);
+router.post("/products/:id/reviews", addReview);
+router.delete("/reviews/:id", deleteReview);
 
 // Wishlist Routes
-router.get('/wishlist', passport.authenticate('jwt', { session: false }), getWishlist); // Protected route
-router.post('/wishlist', passport.authenticate('jwt', { session: false }), addToWishlist); // Protected route
-router.delete('/wishlist/:id', passport.authenticate('jwt', { session: false }), removeFromWishlist); // Protected route
+router.get(
+  "/wishlist",
+  passport.authenticate("jwt", { session: false }),
+  getWishlist
+); // Protected route
+router.post(
+  "/wishlist",
+  passport.authenticate("jwt", { session: false }),
+  addToWishlist
+); // Protected route
+router.delete(
+  "/wishlist/:id",
+  passport.authenticate("jwt", { session: false }),
+  removeFromWishlist
+); // Protected route
 
 // Shipping Routes
-router.post('/shipping', passport.authenticate('jwt', { session: false }), createShipping); // Protected route
-router.put('/shipping/:id', passport.authenticate('jwt', { session: false }), updateShippingStatus); // Protected route
+router.post(
+  "/shipping",
+  passport.authenticate("jwt", { session: false }),
+  createShipping
+); // Protected route
+router.put(
+  "/shipping/:id",
+  passport.authenticate("jwt", { session: false }),
+  updateShippingStatus
+); // Protected route
 
 module.exports = router;
